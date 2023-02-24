f6a3ecca5769376cc63f42b6f694f267fba15073
NRUBYNATHAN <rubynathan999@gmail.com>
2023-02-24 06:26:50 +0530

    port changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 const app = express();
 console.log(process.env. MONGO_URL)
-const PORT = 4000;
+const PORT = process.env.PORT;
 //mongo connection
 // const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env. MONGO_URL;
