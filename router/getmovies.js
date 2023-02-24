be211babd41a14b1b5e01f9ca1c6099d5421c39d
NRUBYNATHAN <rubynathan999@gmail.com>
2023-02-24 09:59:27 +0530

    m

---
+++ router/getmovies.js
@@ -0,0 +1,25 @@
+import { client } from "../index.js";
+
+export async function updatemovies(id, data) {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: data });
+}
+export async function deletemovies(id) {
+  return await client.db("b42wd2").collection("movies").deleteOne({ id: id });
+}
+export async function createmovies(data) {
+  return await client.db("b42wd2").collection("movies").insertMany(data);
+}
+export async function getmoviesbyid(id) {
+  return await client.db("b42wd2").collection("movies").findOne({ id: id });
+}
+export async function getmovies() {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    //Curser ->pagination(20) \ curser -> array =>toArray()
+    .find({})
+    .toArray();
+}
