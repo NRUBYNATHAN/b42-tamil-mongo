dbba58afc95b3355605116e75a886dfa2ef2faa1
NRUBYNATHAN <rubynathan999@gmail.com>
2023-02-28 19:24:07 +0530

    found

--- router/movies.service.js
+++ router/movies.service.js
@@ -10,10 +10,10 @@ export async function deletemovies(id) {
   return await client.db("b42wd2").collection("movies").deleteOne({ id: id });
 }
 export async function createmovies(data) {
-  return await client.db("b42wd2").collection("movies").insertOne(data);
+  return await client.db("b42wd2").collection("movies").insertOne({data});
 }
 export async function getmoviesbyid(id) {
-  return await client.db("b42wd2").collection("movies").findOne({ id: id });
+  return await client.db("b42wd2").collection("movies").findOne({ id:id });
 }
 export async function getmovies() {
   return await client
